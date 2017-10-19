6b9096fcb0f3213b8693c5a13227d0b2f116eb69
johnmaxwellgraham <johnmaxwellgraham@gmail.com>
2017-10-19 10:44:30 -0600

    react

---
+++ public/app.js
@@ -0,0 +1,38 @@
+class MovieList extends React.Component {
+  
+    constructor() {
+      super();
+      this.state = {
+        movies: []
+      };
+    }
+  
+    componentWillMount() {
+      axios.get('/api/moviess')
+        .then((response) => {
+          this.setState({
+            movies: response.data
+          })
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  
+    render() {
+      let movieItems = this.state.movies.map( (movie) => {
+        return <li>{ movie.title }</li>
+      });
+      
+      return (
+        <ul>
+          { movieItems }
+        </ul>
+      );
+    }
+  }
+  
+  ReactDOM.render(
+    <MovieList />,
+    document.getElementById('movies')
+  );
